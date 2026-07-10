43f14906cb326741d3819d2889428bdb4aaeb4dc
Yenuli Minsadi <samararatney@gmail.com>
2026-07-10 13:18:08 +0530

    update with eureka server, api gateway, spring initializr configs and properties.yml file for eureka server instead of application.properties file

---
+++ eureka-server/src/test/java/lk/ijse/supermarket/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.supermarket.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
