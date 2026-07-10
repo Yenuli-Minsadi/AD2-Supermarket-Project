36489271c01d22e55f3fed981c98b67176eec775
Yenuli Minsadi <samararatney@gmail.com>
2026-07-10 12:05:36 +0530

    initial commit with project structure and spring initializr configs

---
+++ customer-service/src/test/java/lk/ijse/supermarket/customerservice/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.supermarket.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ product-service/src/test/java/lk/ijse/supermarket/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.supermarket.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
